f0cde2fc71386cf25678ec51c7094dbf5042087b
unknown <dhileepkumar441@gmail.com>
2023-09-16 22:06:19 +0530

    first commit

---
+++ js/firebaseConfig.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyCLEAvbFhT3O5CN2yuVutWvg2mO4GdpoIM",
+  authDomain: "carshop-3c427.firebaseapp.com",
+  projectId: "carshop-3c427",
+  storageBucket: "carshop-3c427.appspot.com",
+  messagingSenderId: "648161626592",
+  appId: "1:648161626592:web:d2bd39f33025e6720e0385",
+  databaseURL:"https://carshop-3c427-default-rtdb.firebaseio.com/"
+  };
+  firebase.initializeApp(config);
